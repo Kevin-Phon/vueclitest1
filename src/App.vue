260957eccabb7e4c23875687011f5d96515d1ecd
Kevin <kayaswp@gmail.com>
2022-03-05 13:10:41 +0630

    props

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hi {{name}}</h1>
+  <MultiModal head="Login Success" content="Welcome,Traveller" theme="success" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MultiModal from "./components/MultiModel.vue"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      name : "Kevin"
+    }
+  },
+  components:{
+    MultiModal
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  h1{
+    color: aqua;
+  }
 </style>
